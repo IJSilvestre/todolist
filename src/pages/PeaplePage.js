042bf3ada9480aa950761ae2a7d92bdf8f8aca4e
IrleySilvestre <irleysilvestre.mba@gmail.com>
2024-05-25 23:27:11 -0300

    corrigindo list

---
+++ src/pages/PeaplePage.js
@@ -0,0 +1,43 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import React from "react";
+
+import Header from "../components/Header";
+import PeopleList from "../components/PeopleList";
+
+export default class PeaplePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pessoas: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://randomuser.me/api/?nat=br&results=5").then((res) => {
+      const { results } = res.data;
+      this.setState({
+        pessoas: results,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <View style={this.styles.container}>
+        <Header title="Pessoas" />
+        <View>
+          <PeopleList pessoas={this.state.pessoas} />
+        </View>
+      </View>
+    );
+  }
+
+  styles = StyleSheet.create({
+    container: {
+      padding: 5,
+      margin: 5,
+      marginTop: 15,
+    },
+  });
+}
